3854fc6a9f5c28a5f7f95b471bdce4a5fda887d1
mhs8910 <shafiqhassan429@gmail.com>
2026-02-20 00:19:22 +0500

    .

---
+++ Week 2/Write a c++ program to print the following(Cow)/Cow.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+system("color 87");
+cout<<"--------------------------------\n";
+cout<<"\n";
+cout<<"\t o    ^__^\n";
+cout<<"\t  o   (oo)\\___________ \n";
+cout<<"\t      (__)\\          )\\/\\\n";
+cout<<"\t          ||-------w  |\n";
+cout<<"\t          ||         ||\n";
+
+}
